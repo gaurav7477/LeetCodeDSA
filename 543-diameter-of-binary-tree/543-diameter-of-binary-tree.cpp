a7809b8bdf882c959126a1e3f91f7470d65fcae7
GAURAV BAIRAGI <90743990+gaurav7477@users.noreply.github.com>
2022-09-14 21:50:12 +0530

    Time: 23 ms (31.08%), Space: 20.3 MB (65.02%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
@@ -9,40 +9,31 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     int diameterOfBinaryTree(TreeNode* root) {
+        
+//     }
+// };
+
+
+
 class Solution {
 public:
-    // T.C N^2
-    
-    int height(TreeNode * root){
-         if(root == NULL){
-            return 0;
+    int solve(TreeNode* root,int & answer){
+        if(root == NULL) return 0;
+        int left_cnt = solve(root->left,answer);
+        int right_cnt = solve(root->right,answer);
+        if(left_cnt + right_cnt > answer){
+            answer = left_cnt + right_cnt;
         }
-        int left = height(root->left);
-        int right = height(root->right);
-        int ans = max(left,right)+1;
-        return ans;
-    }
-    //
-    // best approach -> remove function call for height of bt
-    pair<int,int> fast(TreeNode* node){
-        if(node == NULL){
-            pair<int,int> p = make_pair(0,0);
-            return p;
-        }
-        pair<int,int> left = fast(node->left);
-        pair<int,int> right = fast(node->right);
+        return max(left_cnt,right_cnt)+1;
         
-        int op1 = left.first;
-        int op2 = right.first;
-        int op3 = left.second + right.second; 
         
-        pair<int,int>ans;
-        ans.first = max(op1,max(op2,op3));
-        ans.second = max(left.second,right.second) + 1;
-        
-        return ans;
     }
     int diameterOfBinaryTree(TreeNode* root) {
-        return fast(root).first;
+        int answer =0;
+         solve(root,answer);
+        return answer;
     }
 };
