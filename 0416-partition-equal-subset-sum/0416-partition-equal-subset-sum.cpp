803fede06a099757d45b52f4be7a2b98b5157254
GAURAV BAIRAGI <90743990+gaurav7477@users.noreply.github.com>
2022-10-13 09:46:42 +0530

    Time: 146 ms (89.12%), Space: 11.8 MB (77.53%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,44 +1,47 @@
 class Solution {
 public:
-    int dp[201][20001];
+    
+    // memoization
+    // int dp[201][20001];
     bool solve(vector<int> &nums, int n, int sum)
     {
-        if (sum == 0)
-            return 1;
-        if (n == 0 && sum > 0)
-            return 0;
-        if (dp[n][sum] != -1)
-            return dp[n][sum];
+//         if (sum == 0)
+//             return 1;
+//         if (n == 0 && sum > 0)
+//             return 0;
+//         if (dp[n][sum] != -1)
+//             return dp[n][sum];
         
-        if (nums[n-1] > sum)
-            return dp[n][sum] = solve(nums, n-1, sum);
-        else
-            return dp[n][sum] = solve(nums, n-1, sum) || solve(nums, n-1, sum-nums[n-1]);
-    }
-        // initialization ->> base condition
-        
-//         for(int i = 0; i < n+1; i++){
-//             for(int j = 0; j < sum+ 1;j++){
-//                 if(i == 0) t[i][j] = false;
-//                 if(j == 0) t[i][j] = true;
+//         if (nums[n-1] > sum)
+//             return dp[n][sum] = solve(nums, n-1, sum);
+//         else
+//             return dp[n][sum] = solve(nums, n-1, sum) || solve(nums, n-1, sum-nums[n-1]);
+//     }
+    
+    // DP solution
+        bool t[n+1][sum+1];
+        for(int i = 0; i < n+1; i++){
+            for(int j = 0; j < sum+ 1;j++){
+                if(i == 0) t[i][j] = false;
+                if(j == 0) t[i][j] = true;
                 
-//             }
-//         }
+            }
+        }
         
-//           for(int i=1; i<n+1; i++){
-//             for(int j=1; j<sum+1; j++){
-//                 if(nums[i-1]<=j){
-//                     t[i][j] = t[i-1][j] || t[i-1][j-nums[i-1]];
-//                 } else{
-//                     t[i][j] = t[i-1][j];
-//                 }
-//             }
-//         }
-//         return t[n][sum];
+          for(int i=1; i<n+1; i++){
+            for(int j=1; j<sum+1; j++){
+                if(nums[i-1]<=j){
+                    t[i][j] = t[i-1][j] || t[i-1][j-nums[i-1]];
+                } else{
+                    t[i][j] = t[i-1][j];
+                }
+            }
+        }
+        return t[n][sum];
      
-    // }
+    }
     bool canPartition(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
+        // memset(dp,-1,sizeof(dp));
         int sum = 0;
         int n = nums.size();
         for(int i =0;i<n;i++){
