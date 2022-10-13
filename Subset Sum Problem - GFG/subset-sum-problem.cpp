1c8561e066a62c18316eef36316e19a7163ee3de
GAURAV BAIRAGI <90743990+gaurav7477@users.noreply.github.com>
2022-10-13 09:18:39 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.cpp
+++ Subset Sum Problem - GFG/subset-sum-problem.cpp
@@ -11,36 +11,58 @@ class Solution{
     
 public:
 
-    bool solve(vector<int>arr, int sum,int n){
-        bool dp[n+1][sum+1];
-        // base case
-        for (int i = 0; i <= n; i++)
-            dp[i][0] = true;
-         for (int i = 1; i <= sum; i++)
-            dp[0][i] = false;
+    // bool solve(vector<int>arr, int sum,int n){
+    //     bool dp[n+1][sum+1];
+    //     // base case
+    //     for (int i = 0; i <= n; i++)
+    //         dp[i][0] = true;
+    //      for (int i = 1; i <= sum; i++)
+    //         dp[0][i] = false;
 
-        for(int i =1; i< n + 1;i++){
-            for(int j =1; j< sum + 1;j++){
-                if(arr[i-1] > j){
-                    dp[i][j] = dp[i-1][j];
+    //     for(int i =1; i< n + 1;i++){
+    //         for(int j =1; j< sum + 1;j++){
+    //             if(arr[i-1] > j){
+    //                 dp[i][j] = dp[i-1][j];
                     
                     
-                }
-                else{
-                    dp[i][j] = dp[i-1][j] || dp[i-1][j-arr[i-1]];
-                }
-            }
+    //             }
+    //             else{
+    //                 dp[i][j] = dp[i-1][j] || dp[i-1][j-arr[i-1]];
+    //             }
+    //         }
             
-        }
-        return dp[n][sum];
+    //     }
+    //     return dp[n][sum];
     
         
-    }
+    // }
 
-    int isSubsetSum(vector<int>arr, int sum){
-        int n = arr.size();
-        if(solve(arr,sum,n)) return 1;
-        else return 0;
+        
+    bool isSubsetSum(vector<int>& nums,int sum){
+        int n = nums.size();
+        bool t[n + 1][sum + 1];
+        
+        // initialization ->> base condition
+        
+        for(int i = 0; i < n+1; i++){
+            for(int j = 0; j < sum+ 1;j++){
+                if(i == 0) t[i][j] = false;
+                if(j == 0) t[i][j] = true;
+                
+            }
+        }
+        
+          for(int i=1; i<n+1; i++){
+            for(int j=1; j<sum+1; j++){
+                if(nums[i-1]<=j){
+                    t[i][j] = t[i-1][j] || t[i-1][j-nums[i-1]];
+                } else{
+                    t[i][j] = t[i-1][j];
+                }
+            }
+        }
+        return t[n][sum];
+     
     }
 };
 
