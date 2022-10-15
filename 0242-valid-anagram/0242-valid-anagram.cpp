5ae0349fb86a8cc05590c4d33ff6fb42fb94e9ea
GAURAV BAIRAGI <90743990+gaurav7477@users.noreply.github.com>
2022-10-15 10:21:00 +0530

    Time: 6 ms (93.89%), Space: 7.5 MB (13.34%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,10 +1,22 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if( t == s)
-            return true;
-        else return false;
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
+        // if( t == s)
+        //     return true;
+        // else return false;
+        unordered_map<char,int>mp;
+        if(s.length() != t.length()) return false;
+        for(int i = 0;i<s.length();i++){
+            mp[s[i]]++;
+            mp[t[i]]--;
+        }
+        
+        for(auto i: mp){
+            if(i.second) return false;
+        }
+        
+        return true;
     }
 };
