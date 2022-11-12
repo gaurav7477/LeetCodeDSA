a7afc6e726f8200a1544ea1302d07e52f14154cb
GAURAV BAIRAGI <90743990+gaurav7477@users.noreply.github.com>
2022-11-12 23:50:20 +0530

    Time: 687 ms (60.12%), Space: 117 MB (65.05%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,28 +1,52 @@
 class MedianFinder {
 public:
-    priority_queue<int>max_heap;
-    priority_queue<int,vector<int>,greater<int>> min_heap;
+    priority_queue<int>max_heap; // store lower element
+    priority_queue<int,vector<int>,greater<int>> min_heap; // store higher element
     
     MedianFinder() {
         
     }
     
     void addNum(int num) {
-        max_heap.push(num);
-        min_heap.push(max_heap.top());
-        max_heap.pop();
-        if(max_heap.size()< min_heap.size()){
-            max_heap.push(min_heap.top());
-            min_heap.pop();
+        if(max_heap.empty()){
+             max_heap.push(num);
+            return;
+        }
+        if(max_heap.size()> min_heap.size()){
+            if(max_heap.top() > num){
+                min_heap.push(max_heap.top());
+                max_heap.pop();
+                max_heap.push(num);
+            }
+            else{
+                min_heap.push(num);
+            }
+        }
+        
+        else{
+            if(min_heap.top() < num){
+                max_heap.push(min_heap.top());
+                min_heap.pop();
+                min_heap.push(num);
+            }
+            else{
+                max_heap.push(num);
+            }
         }
     }
+
     
     double findMedian() {
-        if(max_heap.size()> min_heap.size()){
-            return max_heap.top();
-        }
-        else{
+        if(max_heap.size()== min_heap.size())
+        {
             return (double)(min_heap.top() + max_heap.top())/2.0;
+        }
+        else
+        {
+            if(max_heap.size() > min_heap.size())
+                return max_heap.top();
+            else
+                return min_heap.top();
         }
     }
 };
