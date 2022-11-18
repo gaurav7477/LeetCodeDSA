7cfcd6f4ebe01de14f4ee5ba472057997f4ef6d1
GAURAV BAIRAGI <90743990+gaurav7477@users.noreply.github.com>
2022-11-18 22:26:46 +0530

    Time: 193 ms (87.95%), Space: 71.2 MB (71.84%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        unordered_map<int, int> mp;
+        int i=0, j=0;
+        
+        
+        
+        int mx=1;
+        int k = 2;
+        
+        while(j<fruits.size()){
+            mp[fruits[j]]++;
+            
+            if(mp.size()<k){
+                mx=max(mx, j-i+1);
+                j++;
+            }else if(mp.size()==k){
+                mx=max(mx, j-i+1);
+                j++;
+            }else{
+                while(mp.size()>k){
+                    mp[fruits[i]]--;
+                    if(mp[fruits[i]]==0)
+                        mp.erase(fruits[i]);
+                    
+                    i++;
+                }
+                // mx=max(mx, j-i+1);
+                
+                j++;
+            }
+        }
+        
+        return mx;
+        
+    }
+};
